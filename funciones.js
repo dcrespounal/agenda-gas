6ddb287fa85f82bf3c4e79fa8b4078f50c926960
dcrespounal <dcrespo@unal.edu.co>
2023-05-08 15:21:28 -0500

    video9 GAS avanzado

--- funciones.js
+++ funciones.js
@@ -7,3 +7,10 @@ function obtenerDatosHTML(nombre){
 
     return HtmlService.createHtmlOutputFromFile(nombre).getContent() 
 }
+
+function obtenerContactos(){
+
+    let hoja = SpreadsheetApp.openById('1Q6ARghboL7E9ancmPeJkO3LyOrpzcWpqzs1Vq4P_mIo').getActiveSheet()
+    let datos = hoja.getDataRange().getValues()
+    return datos
+}
