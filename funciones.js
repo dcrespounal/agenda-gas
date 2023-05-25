28a5f85ad13b3e71efcba079d097bc4ba66df045
dcrespounal <dcrespo@unal.edu.co>
2023-05-25 08:56:59 -0500

    video28 curso-gas-avanzado

--- funciones.js
+++ funciones.js
@@ -17,31 +17,29 @@ function obtenerDatosHTML(nombre){
     return HtmlService.createHtmlOutputFromFile(nombre).getContent() 
 }
 
-function obtenerContactos(){
+function obtenerDatos(){
     
     return HOJA.getDataRange().getValues()
 }
 
 function insertarContacto(contacto, imagen){
     //Si se recibe una imagen
-    if(imagen){
-        let blob = Utilities.newBlob(imagen.datos, imagen.tipo, imagen.nombre)
-        let archivo = CARPETA.createFile(blob)
-        contacto.imagen = CABECERA_URL_IMAGEN_DRIVE+archivo.getId()
-    }
+    if(imagen) contacto.imagen = guardarImagen(imagen)
     //añadir un vector con cada celda a añadir
     HOJA.appendRow([contacto.nombre, contacto.apellidos, contacto.correo, contacto.telefono, contacto.imagen])
 }
 
-function borrarContacto(numeroFila){
+function modificarContacto(contacto, imagen){
 
-    HOJA.deleteRow(numeroFila)
+    if(imagen) contacto.imagen = guardarImagen(imagen)
+    let celdas = HOJA.getRange('A'+contacto.fila+':E'+contacto.fila)
+    celdas.setValues([[contacto.nombre, contacto.apellidos, contacto.correo, contacto.telefono, contacto.imagen]])
 }
 
-function modificarContacto(numeroFila, datos){
-
-    let celdas = HOJA.getRange('A'+numeroFila+':D'+numeroFila)
-    celdas.setValues([[datos.nombre, datos.apellidos, datos.correo, datos.telefono]])
+function guardarImagen(imagen){
+    let blob = Utilities.newBlob(imagen.datos, imagen.tipo, imagen.nombre)
+    let archivo = CARPETA.createFile(blob)
+    return CABECERA_URL_IMAGEN_DRIVE+archivo.getId()
 }
 //Importar contactos desde API externa
 function importarContactos(){
@@ -55,9 +53,16 @@ function importarContactos(){
     datos.results.forEach(insertarContactoJSON);
 }
 
+function borrarContacto(numeroFila){
+
+    HOJA.deleteRow(numeroFila)
+}
+
 function insertarContactoJSON(contacto){
 
     HOJA.appendRow([contacto.name.first, contacto.name.last, contacto.email, contacto.phone, contacto.picture.large])
 }
+
+
 
 
